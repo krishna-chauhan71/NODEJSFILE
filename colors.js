d33c6de434950530a92440dda0bff84e778ec65c
Krishna chauhan <chauhan2024k@gmail.com>
2026-06-21 23:02:41 +0530

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+console.log(colors.red("hii this is krishna chauhan."));
+
+console.log(colors.green("hii this is krishna chauhan."));
+
+console.log(colors.yellow("hii this is krishna chauhan."));
